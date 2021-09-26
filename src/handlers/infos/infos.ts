0a0cdf4c8dcf36e59dedbb8ad852e792c4b9070e
Darlan H <darlanhermessjb@hotmail.com>
2021-09-26 20:05:04 -0300

    fix: better guild info message

--- src/handlers/infos/infos.ts
+++ src/handlers/infos/infos.ts
@@ -1,6 +1,8 @@
 import { Message } from 'discord.js';
+import client from '../../client';
 import IGuildRepository from '../../repositories/IGuildRepository';
 import IUserRepository from '../../repositories/IUserRepository';
+import MembersRole from '../../shared/consts/membersRole';
 import BaseHandler from '../../shared/logic/BaseHandler';
 import Handler from '../../shared/logic/Handler';
 
@@ -30,6 +32,24 @@ export default class GuildInfosHandler extends BaseHandler implements Handler {
             return message.reply('Que situação... não conseguimos achar as infos da sua guilda');
         }
 
-        return message.channel.send(`Nome: ${guild.name} \nQuantidade de membros: ${guild.members.length}`);
+        const admins = guild.members.filter(m => m.role === MembersRole.MODERATOR);
+        const owner = guild.members.find(m => m.role === MembersRole.OWNER);
+
+        const adminsUsers = await Promise.all(admins.map(async m => client.users.fetch(m.id)));
+        const ownerUser = await client.users.fetch(owner?.id as string);
+
+        const adminsStr = adminsUsers.map(admin => `${admin.username}`).join('\n');
+
+        let guildFinalStr = `**Nome**: ${guild.name}\n`;
+
+        guildFinalStr += `**Criador**: ${ownerUser.username}\n\n`;
+
+        if (admins.length) {
+            guildFinalStr += `**Admins**\n${adminsStr}\n\n`;
+        }
+
+        guildFinalStr += `Quantidade total de membros: ${guild.members.length}`;
+
+        return message.channel.send(guildFinalStr);
     }
 }
